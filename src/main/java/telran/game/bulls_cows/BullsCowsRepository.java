9e933866f050bbcc3523ee9e4354b01146d4d031
Vitaly Trakhtenberg <webdev@tca.by>
2025-01-11 15:35:21 +0200

    Added some CRUD operations

--- src/main/java/telran/game/bulls_cows/BullsCowsRepository.java
+++ src/main/java/telran/game/bulls_cows/BullsCowsRepository.java
@@ -1,7 +1,9 @@
 package telran.game.bulls_cows;
 
 import telran.game.bulls_cows.common.SessionToken;
-import telran.game.bulls_cows.exceprions.UsernameNotFoundException;
+import telran.game.bulls_cows.exceprions.*;
+
+import java.util.List;
 
 public interface BullsCowsRepository
 {
@@ -11,4 +13,16 @@ public interface BullsCowsRepository
     boolean isGameStarted(int gameId);
     boolean isGameFinished(int gameId);
     boolean isGameHasGamers(int gameId);
+
+    Gamer getGamer(SessionToken gamerToken) throws UsernameNotFoundException;
+    List<Gamer> getAllGamers();
+    List<Game> findGamesOfGamer(SessionToken gamerToken) throws UsernameNotFoundException;
+    Game getGame(int gameId) throws GameNotFoundException;
+    List<Game> getAllGames();
+    List<Game> findAllStartedGames();
+    List<Game> findAllFinishedGames();
+    List<Gamer> findAllGamersOfGame(int gameId) throws GameNotFoundException;
+    int CreateGame();
+    List<Gamer> addGamersToGame(int gameId, List<Gamer> gamers) throws GameNotFoundException, UsernameNotFoundException;
+    boolean removeGamerFromGame(SessionToken gamerToken, int gameId) throws GameNotFoundException, UsernameNotFoundException;
 }
